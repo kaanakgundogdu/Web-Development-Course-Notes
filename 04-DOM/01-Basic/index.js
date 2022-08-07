bd7cc645bc29c5f9641cb83dc77dea9181cf28e0
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-08-07 11:40:45 +0300

    Dom folder added.

---
+++ 04-DOM/01-Basic/index.js
@@ -0,0 +1,15 @@
+document.querySelector("button").style.backgroundColor="red";
+
+
+document.querySelectorAll("li")[1].classList.add("invisible");
+
+// document.querySelector("li a").classList.remove("invisible");
+// document.querySelector("li a").classList.toggle("invisible"); // if there is remove, if there isnt add
+
+document.querySelector("h1").classList.add("huge")
+
+document.querySelector("h2").innerHTML="<em>No More</em>";
+
+console.log(document.querySelector("a").getAttribute("href"));
+
+document.querySelector("a").setAttribute("href","https://www.bing.com")
