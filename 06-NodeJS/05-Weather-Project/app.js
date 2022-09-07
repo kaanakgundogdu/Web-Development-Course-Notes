3fbec6fc3563a20ca54ab458f7b0494b1a60f573
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-09-07 17:15:55 +0300

    Modifeid Weather app.

--- 06-NodeJS/05-Weather-Project/app.js
+++ 06-NodeJS/05-Weather-Project/app.js
@@ -1,32 +1,39 @@
 const express=require("express")
 const https=require("https")
+const bodyParser=require("body-parser")
 const app=express()
 
-const apiKey="---------YOUR API KEY------"
-const query= "Istanbul"
-const unit = "metric"
+app.use(bodyParser.urlencoded({extended: true}))
 
-const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+apiKey;
 
 app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html")
+});
+
+app.post("/",function(req,res){
+    const query= req.body.cityName;
+    const unit = "metric";
+    const apiKey="";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+apiKey;
+    
     https.get(url,function(response){
-        console.log(response.statusCode);
-        response.on("data",function(data){
-            res.setHeader("Content-Type", "text/html");
+    console.log(response.statusCode);
+    response.on("data",function(data){
+        res.setHeader("Content-Type", "text/html");
 
-            const weatherData=JSON.parse(data);
-            const temp=weatherData.main.temp
-            const description=weatherData.weather[0].description;
-            const icon=weatherData.weather[0].icon
-            const imgUrl="http://openweathermap.org/img/wn/"+icon +"@2x.png"
-            res.write("The weather is currently " + description +" \n");  
-            res.write("<h1>The temperature in Istanbul is " + temp + " degree Celcius. </h1>")
-            res.write("<img src=" + imgUrl + ">");
-            
-            res.send()
+        const weatherData=JSON.parse(data);
+        const temp=weatherData.main.temp
+        const description=weatherData.weather[0].description;
+        const icon=weatherData.weather[0].icon
+        const imgUrl="http://openweathermap.org/img/wn/"+icon +"@2x.png"
+        res.write("The weather is currently " + description +" \n");  
+        res.write("<h1>The temperature in "+ query+" is " + temp + " degree Celcius. </h1>")
+        res.write("<img src=" + imgUrl + ">");
+        
+        res.send()
 
-        })
-    });
+    })
+});
 });
 
 
