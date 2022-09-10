a12071f7741575d28f9e263da4a21ad40d3b9c85
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-09-10 14:50:12 +0300

    Request module added. Signup.html modified.

---
+++ 06-NodeJS/06-Sign-Up-Page/app.js
@@ -0,0 +1,10 @@
+const bodyParser= require("body-parser")
+const express=require("express")
+const request= require("request")
+
+let app = express()
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+})
+
