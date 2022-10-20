527d7cb40a056113ca4270edb6c7f3af85aabfbb
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-10-20 14:53:55 +0300

    app.js modified.

--- 06-NodeJS/06-Sign-Up-Page/app.js
+++ 06-NodeJS/06-Sign-Up-Page/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 mailchimp.setConfig({
 //*****************************ENTER YOUR API KEY HERE******************************
-    apiKey: "a8067258e09dfb3aebe39980da97798f-us11",
+    apiKey: "-us11",
 //*****************************ENTER YOUR API KEY PREFIX HERE i.e.THE SERVER******************************
     server: "us11"
 });
@@ -29,7 +29,7 @@ app.post("/",function(req,res){
     const email=req.body.email
 
     /*******************PUT YOUR LIST ID HERE**************************/
-    const listId = "28fe32de19"
+    const listId = ""
 
     const subscribingUser = {
         firstName: firstName,
